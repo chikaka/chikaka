1abc87e93ff90a21582f256d089ae161d8226efc
chedi <chedi@localhost>
2012-12-19 13:24:42 +0100

    Updating the role model:  * Removing the circular reference to the users

--- chikaka/src/main/java/tn/espritcs/chikaka/model/authentification/SystemRole.java
+++ chikaka/src/main/java/tn/espritcs/chikaka/model/authentification/SystemRole.java
@@ -1,9 +1,7 @@
 package tn.espritcs.chikaka.model.authentification;
 
-import java.util.Set;
 import javax.persistence.Id;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.validation.constraints.NotNull;
@@ -15,15 +13,10 @@ public class SystemRole {
 	private int id;
 	
 	@NotNull private String name;
-	
-	@OneToMany(mappedBy = "role")
-	private Set<SystemUser> users;
 
-	public int       getId   () {return id   ;}
-	public String    getName () {return name ;}
-	public Set<SystemUser> getUsers() {return users;}
+	public int             getId   () {return id   ;}
+	public String          getName () {return name ;}
 	
-	public void setId   (int       id   ) {this.id    = id   ;}
-	public void setName (String    name ) {this.name  = name ;}
-	public void setUsers(Set<SystemUser> users) {this.users = users;}
+	public void setId   (int             id   ) {this.id    = id   ;}
+	public void setName (String          name ) {this.name  = name ;}
 }
