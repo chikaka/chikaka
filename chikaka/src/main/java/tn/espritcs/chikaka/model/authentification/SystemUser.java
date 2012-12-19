103b9cfe23b76664fd15d13483539cd0e4395daa
chedi <chedi@localhost>
2012-12-19 18:05:14 +0100

    Hiding the password from the model (removing the getter) adding unique constraint to the user name

--- chikaka/src/main/java/tn/espritcs/chikaka/model/authentification/SystemUser.java
+++ chikaka/src/main/java/tn/espritcs/chikaka/model/authentification/SystemUser.java
@@ -1,12 +1,13 @@
 package tn.espritcs.chikaka.model.authentification;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
 import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
-import javax.validation.constraints.NotNull;
+import org.jboss.security.auth.spi.Util;
 
 @Entity
 public class SystemUser{
@@ -14,8 +15,10 @@ public class SystemUser{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	@NotNull private String userName;
-	@NotNull private String password;
+	@Column(length=50, unique=true, nullable=false)
+	private String userName;
+	@Column(length=300, nullable=false)
+	private String password;
 	
 	@ManyToOne
 	@JoinColumn(name = "role")
@@ -24,10 +27,20 @@ public class SystemUser{
 	public int        getId      () {return id      ;}
 	public SystemRole getRole    () {return role    ;}
 	public String     getUserName() {return userName;}
-	public String     getPassword() {return password;}
 	
 	public void setId      (int        id      ) {this.id       = id      ;}
 	public void setRole    (SystemRole role    ) {this.role     = role    ;}
 	public void setUserName(String     userName) {this.userName = userName;}
-	public void setPassword(String     password) {this.password = password;}
+	
+	private String hashPassword(String password){
+		return Util.createPasswordHash("SHA-256", "BASE64", null, null, password);
+	}
+	
+	public void changePassword(String password){
+		this.password = hashPassword(password);
+	}
+	
+	public boolean checkPassword(String password){
+		return hashPassword(password).equals(this.password);
+	}
 }
