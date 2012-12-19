3d673b3ae342e72980c28b80eaa060424fe8ea19
chedi <chedi@localhost>
2012-12-19 18:04:00 +0100

    Formating update Adding unique constraint to the Role name

--- chikaka/src/main/java/tn/espritcs/chikaka/model/authentification/SystemRole.java
+++ chikaka/src/main/java/tn/espritcs/chikaka/model/authentification/SystemRole.java
@@ -1,10 +1,10 @@
 package tn.espritcs.chikaka.model.authentification;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.validation.constraints.NotNull;
 
 @Entity
 public class SystemRole {
@@ -12,11 +12,12 @@ public class SystemRole {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	@NotNull private String name;
+	@Column(length=50, nullable=false, unique=true, updatable=false, insertable=false) 
+	private String name;
 
-	public int             getId   () {return id   ;}
-	public String          getName () {return name ;}
+	public int    getId   () {return id   ;}
+	public String getName () {return name ;}
 	
-	public void setId   (int             id   ) {this.id    = id   ;}
-	public void setName (String          name ) {this.name  = name ;}
+	public void setId   (int    id   ) {this.id    = id   ;}
+	public void setName (String name ) {this.name  = name ;}
 }
