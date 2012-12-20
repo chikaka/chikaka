e832bfc5b7c06b8e2763c55e890caa0c5d8a74eb
chedi <chedi@localhost>
2012-12-20 21:58:07 +0100

    Adding game creation service

--- chikaka/src/main/java/tn/espritcs/chikaka/rest/user/GameResourceRESTService.java
+++ chikaka/src/main/java/tn/espritcs/chikaka/rest/user/GameResourceRESTService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -17,6 +19,7 @@ import javax.annotation.security.RolesAllowed;
 
 import tn.espritcs.chikaka.model.wrappers.GameWrapper;
 import tn.espritcs.chikaka.service.GameServices;
+import tn.espritcs.chikaka.util.StatusMessage;
 
 @Path("/user/games")
 @RequestScoped
@@ -31,15 +34,31 @@ public class GameResourceRESTService {
 	@Produces(MediaType.APPLICATION_JSON)
 	@RolesAllowed({"User"})
 	public Response listAllGames() { 
+		ResponseBuilder response = null;
 		List<GameWrapper> games =  gameServices.listAllGames();
-		ResponseBuilder responseBuilder = null;
 		if(games.isEmpty()){
-			responseBuilder = Response.status(Status.NOT_FOUND);
-			responseBuilder.entity("there is no games in progress");
+			response = Response.status(Status.NOT_FOUND);
+			response.entity("there is no games in progress");
 		}else {
-			responseBuilder = Response.ok(games, MediaType.APPLICATION_JSON);
+			response = Response.ok(games, MediaType.APPLICATION_JSON);
 		} 
-		return responseBuilder.build(); 
+		return response.build(); 
 	}
-
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@RolesAllowed({"User"})
+	public Response createGame(GameWrapper game){
+		ResponseBuilder response = null;
+		String userName = securityContext.getUserPrincipal().getName();
+		StatusMessage status = gameServices.createGame(game, userName);
+		if(status.getStatus()){
+			response = Response.status(Status.CREATED);
+		}else{
+			response = Response.status(Status.BAD_REQUEST);
+		}
+		response.entity(status.getMessage());
+		return response.build();
+	}
 }
--- chikaka/src/main/java/tn/espritcs/chikaka/service/GameServices.java
+++ chikaka/src/main/java/tn/espritcs/chikaka/service/GameServices.java
@@ -4,17 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext; 
+
+import tn.espritcs.chikaka.model.game.Account;
 import tn.espritcs.chikaka.model.game.Game;
+import tn.espritcs.chikaka.model.game.Session;
 import tn.espritcs.chikaka.model.wrappers.GameWrapper;
-
-
+import tn.espritcs.chikaka.util.StatusMessage;
 
 @Stateless
 public class GameServices {
 	@PersistenceContext(unitName = "primary")
 	private EntityManager em;
+	
+	@Inject
+	private AccountServices accountServices;
+	
 	public List<GameWrapper> listAllGames(){
 		@SuppressWarnings("unchecked")
 		final List<Game> games = em.createQuery("select g from Game g").getResultList();
@@ -23,5 +30,19 @@ public class GameServices {
 			results.add(new GameWrapper(game));
 		}
 		return results;
+	}
+
+	public StatusMessage createGame(GameWrapper game, String userName) {
+		try{
+			Account account = accountServices.lookupAccountByUserName(userName).toAccount();
+			Session session = account.getActiveSession();
+			Game newGame = game.toGame();
+			em.persist(newGame);
+			session.setGame(newGame);
+			em.persist(session);
+			return new StatusMessage(true, "Created");
+		}catch(Exception e){
+			return new StatusMessage(false, e.getMessage());
+		}
 	} 
 }
