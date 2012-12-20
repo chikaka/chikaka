bb0ac2103e114fef11f3be184de425efc038fa75
chedi <chedi@localhost>
2012-12-20 14:26:27 +0100

    adding extra account for test in the import.sql file

--- chikaka/src/main/resources/import.sql
+++ chikaka/src/main/resources/import.sql
@@ -1,15 +1,27 @@
 insert into SystemUser(UserName, Password) Values ('administrator', encode(digest('chikaka', 'sha256'), 'base64'));
-insert into SystemUser(UserName, Password) Values ('chedi', encode(digest('wa333', 'sha256'), 'base64'));
-insert into SystemUser(UserName, Password) Values ('guest', encode(digest('guest', 'sha256'), 'base64'));
+insert into SystemUser(UserName, Password) Values ('chedi'        , encode(digest('wa333'  , 'sha256'), 'base64'));
+insert into SystemUser(UserName, Password) Values ('emir'         , encode(digest('wa333'  , 'sha256'), 'base64'));
+insert into SystemUser(UserName, Password) Values ('adel'         , encode(digest('wa333'  , 'sha256'), 'base64'));
+insert into SystemUser(UserName, Password) Values ('khaled'       , encode(digest('wa333'  , 'sha256'), 'base64'));
+insert into SystemUser(UserName, Password) Values ('guest'        , encode(digest('guest'  , 'sha256'), 'base64'));
 
 insert into SystemRole(name) values ('Admin');
-insert into SystemRole(name) values ('User');
 insert into SystemRole(name) values ('Guest');
+insert into SystemRole(name) values ('User' );
 
 update SystemUser set Role = (select id from SystemRole where name = 'Admin') where UserName = 'administrator'; 
-update SystemUser set Role = (select id from SystemRole where name = 'User' ) where UserName = 'chedi';
-update SystemUser set Role = (select id from SystemRole where name = 'Guest') where UserName = 'guest';
+update SystemUser set Role = (select id from SystemRole where name = 'User' ) where UserName = 'chedi'        ;
+update SystemUser set Role = (select id from SystemRole where name = 'User' ) where UserName = 'emir'         ;
+update SystemUser set Role = (select id from SystemRole where name = 'User' ) where UserName = 'adel'         ;
+update SystemUser set Role = (select id from SystemRole where name = 'User' ) where UserName = 'khaled'       ;
+update SystemUser set Role = (select id from SystemRole where name = 'Guest') where UserName = 'guest'        ;
 
-insert into Account(avatar, email, firstName, lastName) values ('default.png', 'test@test.com', 'douda', 'chikaka');
+insert into Account(avatar, email, firstName, lastName) values ('default.png', 'test0@test.com', 'douda0', 'chikaka0');
+insert into Account(avatar, email, firstName, lastName) values ('default.png', 'test1@test.com', 'douda1', 'chikaka1');
+insert into Account(avatar, email, firstName, lastName) values ('default.png', 'test2@test.com', 'douda2', 'chikaka2');
+insert into Account(avatar, email, firstName, lastName) values ('default.png', 'test3@test.com', 'douda3', 'chikaka3');
 
-update Account set user_id=(select id from SystemUser where UserName='chedi');
+update Account set user_id=(select id from SystemUser where UserName='chedi' ) where firstName = 'douda0';
+update Account set user_id=(select id from SystemUser where UserName='emir'  ) where firstName = 'douda1';
+update Account set user_id=(select id from SystemUser where UserName='adel'  ) where firstName = 'douda2';
+update Account set user_id=(select id from SystemUser where UserName='khaled') where firstName = 'douda3';
